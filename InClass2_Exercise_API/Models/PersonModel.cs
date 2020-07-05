280942519e870b297c23d5e204024c21fb252a35
bibhas05041984 <bibhas.apr5@gmail.com>
2020-07-04 23:58:09 -0400

    test

---
+++ InClass2_Exercise_API/Models/PersonModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web; 
+
+namespace InClass2_Exercise_API.Models
+{
+    public class PersonModel
+    {
+
+        public string Name { get; set; }
+        ///// <summary>
+        ///// Gets or sets City.  
+        ///// </summary>  
+
+        public string City { get; set; }
+        ///// <summary>
+        ///// Gets or sets Gender. 
+        ///// </summary>  
+
+        public string Attending { get; set; }
+
+        private static List<PersonModel> responses = new List<PersonModel>();
+        public static IEnumerable<PersonModel> Responses
+        {
+            get
+            {
+                return responses;
+            }
+        }
+        public static void AddResponse(PersonModel response)
+        {
+            responses.Add(response);
+        }
+ 
+
+
+    }
+}
